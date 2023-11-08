6d68221c45f629b30b357c074552b5975c25ee0b
Eliana Hippler <elianahippler@gmail.com>
2023-11-08 17:39:34 -0300

    Add files via upload

--- js/pedido.js
+++ js/pedido.js
@@ -13,6 +13,8 @@ const bebidas = [
 
 const doces = [
     {id: 7, nome: 'Suco de Laranja', img: '../feira4adm/imagens/produtos/sucodelaranja.jpg', preco: 5.00, quantidade: 0},
+    {id: 8, nome: 'Água com gás', img: '../feira4adm/imagens/produtos/aguagas.jpg', preco: 3.00, quantidade: 0},
+    {id: 9, nome: 'Água sem gás', img: '../feira4adm/imagens/produtos/aguagas.jpg', preco: 3.00, quantidade: 0},
 ];
 
 const allItems = lanches.concat(bebidas, doces);
